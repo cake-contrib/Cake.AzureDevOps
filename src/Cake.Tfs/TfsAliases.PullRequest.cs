dbdfecbd8094f940b6dc48d80501f3c2db91d0ff
Pascal Berger <pascal.berger@gmail.com>
2018-08-22 13:11:30 +0200

    (GH-4) Add alias for retrieving commit ID of pull request

--- src/Cake.Tfs/TfsAliases.PullRequest.cs
+++ src/Cake.Tfs/TfsAliases.PullRequest.cs
@@ -46,5 +46,42 @@
             var pullRequest = new TfsPullRequest(context.Log, settings);
             pullRequest.Vote(vote);
         }
+
+        /// <summary>
+        /// Gets the last commit hash on the source branch of the Team Foundation Server or
+        /// Visual Studio Team Services pull request using the specified settings.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="settings">Settings for accessing the pull request system.</param>
+        /// <example>
+        /// <para>Get the hash of the last commit:</para>
+        /// <code>
+        /// <![CDATA[
+        ///     var pullRequestSettings =
+        ///         new TfsPullRequestSettings(
+        ///             new Uri("http://myserver:8080/tfs/defaultcollection/myproject/_git/myrepository"),
+        ///             "refs/heads/feature/myfeature",
+        ///             TfsAuthenticationNtlm());
+        ///
+        ///     var hash =
+        ///         TfsPullRequestLastSourceCommit(
+        ///             pullRequestSettings);
+        /// ]]>
+        /// </code>
+        /// </example>
+        /// <returns>The hash of the last commit on the source branch or <see cref="string.Empty"/>
+        /// if no pull request could be found.</returns>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Pull Request")]
+        public static string TfsPullRequestLastSourceCommit(
+            this ICakeContext context,
+            TfsPullRequestSettings settings)
+        {
+            context.NotNull(nameof(context));
+            settings.NotNull(nameof(settings));
+
+            var pullRequest = new TfsPullRequest(context.Log, settings);
+            return pullRequest.LastSourceCommitId;
+        }
     }
 }
