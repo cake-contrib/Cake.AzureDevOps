9dd722947a9e71c9509e1d2b25b95ca572c74831
Pascal Berger <pascal.berger@gmail.com>
2019-10-28 22:10:20 +0100

    (GH-169) Add alias for accessing Azure Pipeline builds

---
+++ src/Cake.AzureDevOps/Build/BuildClient/BuildClientFactory.cs
@@ -0,0 +1,46 @@
+﻿namespace Cake.AzureDevOps
+{
+    using System;
+    using Cake.AzureDevOps.Authentication;
+    using Microsoft.TeamFoundation.Build.WebApi;
+    using Microsoft.VisualStudio.Services.Identity;
+    using Microsoft.VisualStudio.Services.WebApi;
+
+    /// <inheritdoc />
+    internal class BuildClientFactory : IBuildClientFactory
+    {
+        /// <summary>
+        /// Creates a client object for communicating with Azure DevOps.
+        /// </summary>
+        /// <param name="collectionUrl">The URL of the Azure DevOps team project collection.</param>
+        /// <param name="credentials">The credentials to connect to Azure DevOps.</param>
+        /// <param name="authorizedIdentity">Returns identity which is authorized.</param>
+        /// <returns>Client object for communicating with Azure DevOps.</returns>
+        public BuildHttpClient CreateBuildClient(Uri collectionUrl, IAzureDevOpsCredentials credentials, out Identity authorizedIdentity)
+        {
+            var connection =
+                new VssConnection(collectionUrl, credentials.ToVssCredentials());
+
+            authorizedIdentity = connection.AuthorizedIdentity;
+
+            var gitClient = connection.GetClient<BuildHttpClient>();
+            if (gitClient == null)
+            {
+                throw new AzureDevOpsException("Could not retrieve the BuildHttpClient object");
+            }
+
+            return gitClient;
+        }
+
+        /// <summary>
+        /// Creates a client object for communicating with Azure DevOps.
+        /// </summary>
+        /// <param name="collectionUrl">The URL of the Azure DevOps team project collection.</param>
+        /// <param name="credentials">The credentials to connect to Azure DevOps.</param>
+        /// <returns>Client object for communicating with Azure DevOps.</returns>
+        public BuildHttpClient CreateBuildClient(Uri collectionUrl, IAzureDevOpsCredentials credentials)
+        {
+            return this.CreateBuildClient(collectionUrl, credentials, out _);
+        }
+    }
+}
