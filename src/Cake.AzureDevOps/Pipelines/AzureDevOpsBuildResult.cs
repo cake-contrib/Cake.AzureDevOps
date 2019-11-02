fddf6e6c6579fda14f4fa47f997f05d7f180ff9a
Pascal Berger <pascal.berger@gmail.com>
2019-11-02 10:02:39 +0100

    (GH-173) Return build result

---
+++ src/Cake.AzureDevOps/Pipelines/AzureDevOpsBuildResult.cs
@@ -0,0 +1,33 @@
+﻿namespace Cake.AzureDevOps.Pipelines
+{
+    /// <summary>
+    /// Possible results of a build.
+    /// </summary>
+    public enum AzureDevOpsBuildResult : byte
+    {
+        /// <summary>
+        /// Result is not set.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The build completed successfully.
+        /// </summary>
+        Succeeded = 2,
+
+        /// <summary>
+        /// The build completed compilation successfully but had other errors.
+        /// </summary>
+        PartiallySucceeded = 4,
+
+        /// <summary>
+        /// The build completed unsuccessfully.
+        /// </summary>
+        Failed = 8,
+
+        /// <summary>
+        /// The build was canceled before starting.
+        /// </summary>
+        Canceled = 32,
+    }
+}
---
+++ src/Cake.AzureDevOps/Pipelines/BuildResultExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Cake.AzureDevOps.Pipelines
+{
+    using Microsoft.TeamFoundation.Build.WebApi;
+
+    /// <summary>
+    /// Extensions for the <see cref="BuildResult"/> class.
+    /// </summary>
+    internal static class BuildResultExtensions
+    {
+        /// <summary>
+        /// Converts a <see cref="BuildResult"/> to an <see cref="AzureDevOpsBuildResult"/>.
+        /// </summary>
+        /// <param name="result">Build result to convert.</param>
+        /// <returns>Converted build result.</returns>
+        public static AzureDevOpsBuildResult ToAzureDevOpsBuildResult(this BuildResult result)
+        {
+            switch (result)
+            {
+                case BuildResult.None:
+                    return AzureDevOpsBuildResult.None;
+                case BuildResult.Succeeded:
+                    return AzureDevOpsBuildResult.Succeeded;
+                case BuildResult.PartiallySucceeded:
+                    return AzureDevOpsBuildResult.PartiallySucceeded;
+                case BuildResult.Failed:
+                    return AzureDevOpsBuildResult.Failed;
+                case BuildResult.Canceled:
+                    return AzureDevOpsBuildResult.Canceled;
+                default:
+                    throw new System.Exception("Unknown value");
+            }
+        }
+    }
+}
