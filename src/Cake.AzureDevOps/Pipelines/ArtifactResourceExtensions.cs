fadbdcc62ecfb42276d62e097364efdd0817a9aa
Christian Bumann <bch@bbtsoftware.ch>
2020-07-08 09:46:21 +0200

    Return artifacts of a build
    Resolves #176

---
+++ src/Cake.AzureDevOps/Pipelines/ArtifactResourceExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace Cake.AzureDevOps.Pipelines
+{
+    using Microsoft.TeamFoundation.Build.WebApi;
+
+    /// <summary>
+    /// Extensions for the <see cref="ArtifactResource"/> class.
+    /// </summary>
+    internal static class ArtifactResourceExtensions
+    {
+        /// <summary>
+        /// Converts a <see cref="ArtifactResource"/> to an <see cref="AzureDevOpsBuildArtifact"/>.
+        /// </summary>
+        /// <param name="artifactResource">Artifact resource record to convert.</param>
+        /// <returns>Converted artifact resorce record.</returns>
+        public static AzureDevOpsArtifactResource ToAzureDevOpsArtifactResource(this ArtifactResource artifactResource)
+        {
+            artifactResource.NotNull(nameof(artifactResource));
+
+            return
+                new AzureDevOpsArtifactResource
+                {
+                    Data = artifactResource.Data,
+                    DownloadUrl = artifactResource.DownloadUrl,
+                    Type = artifactResource.Type,
+                    Url = artifactResource.Url,
+                    Properties = artifactResource.Properties,
+                };
+        }
+    }
+}
---
+++ src/Cake.AzureDevOps/Pipelines/BuildArtifactExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Cake.AzureDevOps.Pipelines
+{
+    using Microsoft.TeamFoundation.Build.WebApi;
+
+    /// <summary>
+    /// Extensions for the <see cref="BuildArtifact"/> class.
+    /// </summary>
+    internal static class BuildArtifactExtensions
+    {
+        /// <summary>
+        /// Converts a <see cref="BuildArtifact"/> to an <see cref="AzureDevOpsBuildArtifact"/>.
+        /// </summary>
+        /// <param name="buildArtifact">Build artifact record to convert.</param>
+        /// <returns>Converted build artifact record.</returns>
+        public static AzureDevOpsBuildArtifact ToAzureDevOpsBuildArtifact(this BuildArtifact buildArtifact)
+        {
+            buildArtifact.NotNull(nameof(buildArtifact));
+
+            return
+                new AzureDevOpsBuildArtifact
+                {
+                    Id = buildArtifact.Id,
+                    Name = buildArtifact.Name,
+                    Resource = buildArtifact.Resource.ToAzureDevOpsArtifactResource(),
+                };
+        }
+    }
+}
