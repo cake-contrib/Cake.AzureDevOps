0a1429d04210fa7daf156285ff7c79afbd21c5e0
Christian Bumann <bch@bbtsoftware.ch>
2020-09-18 10:47:12 +0200

    Add an alias to get all build definitions for a project
    Resolves #265

---
+++ src/Cake.AzureDevOps/Pipelines/DefinitionQueueStatusExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace Cake.AzureDevOps.Pipelines
+{
+    using Microsoft.TeamFoundation.Build.WebApi;
+
+    /// <summary>
+    /// Extensions for the <see cref="DefinitionQueueStatus"/> class.
+    /// </summary>
+    internal static class DefinitionQueueStatusExtensions
+    {
+        /// <summary>
+        /// Converts a <see cref="DefinitionQueueStatus"/> to an <see cref="AzureDevOpsDefinitionQueueStatus"/>.
+        /// </summary>
+        /// <param name="status">Status to convert.</param>
+        /// <returns>Converted status.</returns>
+        public static AzureDevOpsDefinitionQueueStatus ToAzureDevOpsDefinitionQueueStatus(this DefinitionQueueStatus status)
+        {
+            switch (status)
+            {
+                case DefinitionQueueStatus.Enabled:
+                    return AzureDevOpsDefinitionQueueStatus.Enabled;
+                case DefinitionQueueStatus.Paused:
+                    return AzureDevOpsDefinitionQueueStatus.Paused;
+                case DefinitionQueueStatus.Disabled:
+                    return AzureDevOpsDefinitionQueueStatus.Disabled;
+                default:
+                    throw new System.Exception("Unknown value");
+            }
+        }
+    }
+}
