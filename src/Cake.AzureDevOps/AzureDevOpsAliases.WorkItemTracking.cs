d548699c2453b5b4439d0d27c48a763c6b718270
Wenzel, Toni <toni.wenzel@chg-meridian.com>
2022-09-24 18:18:30 +0200

    Initial setup for work items Fixes #360

---
+++ src/Cake.AzureDevOps/AzureDevOpsAliases.WorkItemTracking.cs
@@ -0,0 +1,133 @@
+﻿namespace Cake.AzureDevOps
+{
+    using Cake.AzureDevOps.WorkItemTracking;
+    using Cake.Core;
+    using Cake.Core.Annotations;
+
+    /// <content>
+    /// Contains functionality related to Azure DevOps work item tracking.
+    /// </content>
+    public static partial class AzureDevOpsAliases
+    {
+        /// <summary>
+        /// Gets an Azure DevOps work item using the specified settings.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="settings">Settings for getting the work item.</param>
+        /// <example>
+        /// <para>Get a work item from Azure DevOps Server:</para>
+        /// <code>
+        /// <![CDATA[
+        /// var workItemSettings =
+        ///     new AzureDevOpsWorkItemSettings(
+        ///         new Uri("http://myserver:8080/defaultcollection"),
+        ///         "MyProject",
+        ///         42,
+        ///         AzureDevOpsAuthenticationNtlm());
+        ///
+        /// var workItem =
+        ///     AzureDevOpsWorkItem(
+        ///         workItemSettings);
+        /// ]]>
+        /// </code>
+        /// </example>
+        /// <returns>Description of the work item.
+        /// Returns <c>null</c> if work item could not be found and
+        /// <see cref="AzureDevOpsWorkItemSettings.ThrowExceptionIfWorkItemCouldNotBeFound"/> is set to <c>false</c>.</returns>
+        /// <exception cref="AzureDevOpsWorkItemNotFoundException">If work item could not be found and
+        /// <see cref="AzureDevOpsWorkItemSettings.ThrowExceptionIfWorkItemCouldNotBeFound"/> is set to <c>true</c>.</exception>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Azure DevOps Work Item Tracking")]
+        [CakeNamespaceImport("Cake.AzureDevOps.WorkItemTracking")]
+        public static AzureDevOpsWorkItem AzureDevOpsWorkItem(
+            this ICakeContext context,
+            AzureDevOpsWorkItemSettings settings)
+        {
+            context.NotNull(nameof(context));
+            settings.NotNull(nameof(settings));
+
+            var workItem = new AzureDevOpsWorkItem(context.Log, settings, new WorkItemTrackingClientFactory());
+
+            if (workItem.HasWorkItemLoaded)
+            {
+                return workItem;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the description of a specific work item the access token provided by Azure Pipelines.
+        /// Make sure the build has the 'Allow Scripts to access OAuth token' option enabled.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="workItemId">ID of the work witem.</param>
+        /// <example>
+        /// <para>Get an Azure DevOps work item:</para>
+        /// <code>
+        /// <![CDATA[
+        /// var workItem =
+        ///     AzureDevOpsBuildUsingAzurePipelinesOAuthToken(42);
+        /// ]]>
+        /// </code>
+        /// </example>
+        /// <returns>Description of the work item.
+        /// Returns <c>null</c> if work item could not be found and
+        /// <see cref="AzureDevOpsWorkItemSettings.ThrowExceptionIfWorkItemCouldNotBeFound"/> is set to <c>false</c>.</returns>
+        /// <exception cref="AzureDevOpsWorkItemNotFoundException">If work item could not be found and
+        /// <see cref="AzureDevOpsWorkItemSettings.ThrowExceptionIfWorkItemCouldNotBeFound"/> is set to <c>true</c>.</exception>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Azure DevOps Work Item Tracking")]
+        [CakeNamespaceImport("Cake.AzureDevOps.WorkItemTracking")]
+        public static AzureDevOpsWorkItem AzureDevOpsWorkItemUsingAzurePipelinesOAuthToken(
+            this ICakeContext context,
+            int workItemId)
+        {
+            context.NotNull(nameof(context));
+            workItemId.NotNegativeOrZero(nameof(workItemId));
+
+            var settings = AzureDevOpsWorkItemSettings.UsingAzurePipelinesOAuthToken(workItemId);
+
+            return AzureDevOpsWorkItem(context, settings);
+        }
+
+        /// <summary>
+        /// Gets the description of a specific work item the access token provided by Azure Pipelines.
+        /// Make sure the build has the 'Allow Scripts to access OAuth token' option enabled.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="workItemId">ID of the work item.</param>
+        /// <param name="throwExceptionIfWorkItemCouldNotBeFound">Value indicating whether an exception
+        /// should be thrown if work item could not be found.</param>
+        /// <example>
+        /// <para>Get an Azure DevOps work item. Don't throw exception in case the work item is not found:</para>
+        /// <code>
+        /// <![CDATA[
+        /// var workItem =
+        ///     AzureDevOpsWorkItemUsingAzurePipelinesOAuthToken(42, false);
+        /// ]]>
+        /// </code>
+        /// </example>
+        /// <returns>Description of the work item.
+        /// Returns <c>null</c> if work item could not be found and
+        /// <paramref name="throwExceptionIfWorkItemCouldNotBeFound"/> is set to <c>false</c>.</returns>
+        /// <exception cref="AzureDevOpsWorkItemNotFoundException">If work item could not be found and
+        /// <paramref name="throwExceptionIfWorkItemCouldNotBeFound"/> is set to <c>true</c>.</exception>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Azure DevvOps Work Item Tracking")]
+        [CakeNamespaceImport("Cake.AzureDevOps.WorkItemTracking")]
+        public static AzureDevOpsWorkItem AzureDevOpsWorkItemUsingAzurePipelinesOAuthToken(
+            this ICakeContext context,
+            int workItemId,
+            bool throwExceptionIfWorkItemCouldNotBeFound)
+        {
+            context.NotNull(nameof(context));
+            workItemId.NotNegativeOrZero(nameof(workItemId));
+
+            var settings = AzureDevOpsWorkItemSettings.UsingAzurePipelinesOAuthToken(workItemId);
+            settings.ThrowExceptionIfWorkItemCouldNotBeFound = throwExceptionIfWorkItemCouldNotBeFound;
+
+            return AzureDevOpsWorkItem(context, settings);
+        }
+    }
+}
---
+++ src/Cake.AzureDevOps/WorkItemTracking/AzureDevOpsWorkItem.cs
@@ -0,0 +1,189 @@
+﻿namespace Cake.AzureDevOps.WorkItemTracking
+{
+    using System;
+    using Cake.AzureDevOps.Authentication;
+    using Cake.Core.Diagnostics;
+    using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+    using Microsoft.VisualStudio.Services.Common;
+
+    /// <summary>
+    /// Class to work with work item tracking.
+    /// </summary>
+    public sealed class AzureDevOpsWorkItem
+    {
+        private readonly ICakeLog log;
+        private readonly IAzureDevOpsCredentials credentials;
+        private readonly bool throwExceptionIfWorkItemCouldNotBeFound;
+        private readonly IWorkItemTrackingClientFactory workItemTrackingClientFactory;
+        private readonly WorkItem workItem;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AzureDevOpsWorkItem"/> class.
+        /// </summary>
+        /// <param name="log">The Cake log context.</param>
+        /// <param name="settings">Settings for accessing AzureDevOps.</param>
+        /// <exception cref="AzureDevOpsWorkItemNotFoundException">If <see cref="AzureDevOpsWorkItemSettings.ThrowExceptionIfWorkItemCouldNotBeFound"/>
+        /// is set to <c>true</c> and no work item could be found.</exception>
+        public AzureDevOpsWorkItem(ICakeLog log, AzureDevOpsWorkItemSettings settings)
+            : this(log, settings, new WorkItemTrackingClientFactory())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AzureDevOpsWorkItem"/> class.
+        /// </summary>
+        /// <param name="log">The Cake log context.</param>
+        /// <param name="settings">Settings for accessing AzureDevOps.</param>
+        /// <param name="workItem">The work item.</param>
+        internal AzureDevOpsWorkItem(ICakeLog log, AzureDevOpsWorkItemSettings settings, WorkItem workItem)
+        {
+            log.NotNull(nameof(log));
+            settings.NotNull(nameof(settings));
+            workItem.NotNull(nameof(workItem));
+
+            this.log = log;
+            this.workItem = workItem;
+            this.workItemTrackingClientFactory = new WorkItemTrackingClientFactory();
+            this.credentials = settings.Credentials;
+            this.CollectionUrl = settings.CollectionUrl;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AzureDevOpsWorkItem"/> class.
+        /// </summary>
+        /// <param name="log">The Cake log context.</param>
+        /// <param name="settings">Settings for accessing AzureDevOps.</param>
+        /// <param name="workItemTrackingClientFactory">A factory to communicate with work item tracking client.</param>
+        /// <exception cref="AzureDevOpsWorkItemNotFoundException">If <see cref="AzureDevOpsWorkItemSettings.ThrowExceptionIfWorkItemCouldNotBeFound"/>
+        /// is set to <c>true</c> and no work item could be found.</exception>
+        internal AzureDevOpsWorkItem(
+            ICakeLog log,
+            AzureDevOpsWorkItemSettings settings,
+            IWorkItemTrackingClientFactory workItemTrackingClientFactory)
+        {
+            log.NotNull(nameof(log));
+            settings.NotNull(nameof(settings));
+            workItemTrackingClientFactory.NotNull(nameof(workItemTrackingClientFactory));
+
+            this.log = log;
+            this.workItemTrackingClientFactory = workItemTrackingClientFactory;
+            this.credentials = settings.Credentials;
+            this.CollectionUrl = settings.CollectionUrl;
+            this.throwExceptionIfWorkItemCouldNotBeFound = settings.ThrowExceptionIfWorkItemCouldNotBeFound;
+
+            using (var workItemTrackingClient = this.workItemTrackingClientFactory.CreateWorkItemTrackingClient(settings.CollectionUrl, settings.Credentials, out var authorizedIdenity))
+            {
+                this.log.Verbose(
+                     "Authorized Identity:\n  Id: {0}\n  DisplayName: {1}",
+                     authorizedIdenity.Id,
+                     authorizedIdenity.DisplayName);
+
+                try
+                {
+                    if (settings.ProjectGuid != Guid.Empty)
+                    {
+                        this.log.Verbose("Read work item with ID {0} from project with ID {1}", settings.WorkItemId, settings.ProjectGuid);
+                        this.workItem =
+                            workItemTrackingClient
+                                .GetWorkItemAsync(
+                                    settings.ProjectGuid,
+                                    settings.WorkItemId)
+                                .ConfigureAwait(false)
+                                .GetAwaiter()
+                                .GetResult();
+                    }
+                    else if (!string.IsNullOrWhiteSpace(settings.ProjectName))
+                    {
+                        this.log.Verbose("Read work item with ID {0} from project with name {1}", settings.WorkItemId, settings.ProjectName);
+                        this.workItem =
+                            workItemTrackingClient
+                                .GetWorkItemAsync(
+                                    settings.ProjectName,
+                                    settings.WorkItemId)
+                                .ConfigureAwait(false)
+                                .GetAwaiter()
+                                .GetResult();
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException(
+                            nameof(settings),
+                            "Either ProjectGuid or ProjectName needs to be set");
+                    }
+                }
+                catch (VssServiceException ex)
+                {
+                    if (this.throwExceptionIfWorkItemCouldNotBeFound)
+                    {
+                        throw new AzureDevOpsWorkItemNotFoundException("Work item not found", ex);
+                    }
+
+                    this.log.Warning("Could not find work item");
+                    return;
+                }
+            }
+
+            if (this.workItem != null)
+            {
+                this.log.Verbose(
+                    "Work item information:\n  Id: {0}\n  Url: {1}",
+                    this.workItem.Id,
+                    this.workItem.Url);
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether a work item has been successfully loaded.
+        /// </summary>
+        public bool HasWorkItemLoaded => this.workItem != null;
+
+        /// <summary>
+        /// Gets the URL for accessing the web portal of the Azure DevOps collection.
+        /// </summary>
+        public Uri CollectionUrl { get; }
+
+        /// <summary>
+        /// Gets the ID of the work item.
+        /// Returns 0 if no work item could be found and
+        /// <see cref="AzureDevOpsWorkItemSettings.ThrowExceptionIfWorkItemCouldNotBeFound"/> is set to <c>false</c>.
+        /// </summary>
+        /// <exception cref="AzureDevOpsWorkItemNotFoundException">If work item could not be found and
+        /// <see cref="AzureDevOpsWorkItemSettings.ThrowExceptionIfWorkItemCouldNotBeFound"/> is set to <c>true</c>.</exception>
+        public int WorkItemId
+        {
+            get
+            {
+                if (!this.ValidateWorkItem())
+                {
+                    return 0;
+                }
+
+                return this.workItem.Id ?? 0;
+            }
+        }
+
+        /// <summary>
+        /// Validates if a work item could be found.
+        /// Depending on <see cref="AzureDevOpsWorkItemSettings.ThrowExceptionIfWorkItemCouldNotBeFound"/>
+        /// the work item instance can be null for subsequent calls.
+        /// </summary>
+        /// <returns>True if a valid work item instance exists.</returns>
+        /// <exception cref="AzureDevOpsWorkItemNotFoundException">If <see cref="AzureDevOpsWorkItemSettings.ThrowExceptionIfWorkItemCouldNotBeFound"/>
+        /// is set to <c>true</c> and no work item could be found.</exception>
+        private bool ValidateWorkItem()
+        {
+            if (this.HasWorkItemLoaded)
+            {
+                return true;
+            }
+
+            if (this.throwExceptionIfWorkItemCouldNotBeFound)
+            {
+                throw new AzureDevOpsWorkItemNotFoundException("Work item not found");
+            }
+
+            this.log.Verbose("Skipping, since no work item instance could be found.");
+            return false;
+        }
+    }
+}
