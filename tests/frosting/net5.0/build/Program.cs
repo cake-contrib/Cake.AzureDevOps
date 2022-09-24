9c44bf596554191885e51bd44b76ff97fd3a32a3
Pascal Berger <pascal.berger@swissgrc.com>
2022-09-24 14:23:13 +0000

    Add more integration tests (#359)

--- tests/frosting/net5.0/build/Program.cs
+++ tests/frosting/net5.0/build/Program.cs
@@ -4,6 +4,7 @@ using Cake.Core;
 using Cake.Core.Diagnostics;
 using Cake.Frosting;
 using Cake.AzureDevOps;
+using System.Linq;
 
 public static class Program
 {
@@ -35,6 +36,98 @@ public sealed class ReadBuildTask : FrostingTask<BuildContext>
     }
 }
 
+[TaskName("Read-BuildChanges")]
+public sealed class ReadBuildChangesTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        var build =
+            context.AzureDevOpsBuildUsingAzurePipelinesOAuthToken();
+
+        var changes = build.GetChanges();
+        if (!changes.Any())
+        {
+            context.Information("No changes found.");
+        }
+        else
+        {
+            foreach (var change in changes)
+            {
+                context.Information("{0}: {1} by {2}", change.Id, change.Message, change.Author);
+            }
+        }
+    }
+}
+
+[TaskName("Read-BuildTimelineRecords")]
+public sealed class ReadBuildTimelineRecordsTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        var build =
+            context.AzureDevOpsBuildUsingAzurePipelinesOAuthToken();
+
+        var timelineRecords = build.GetTimelineRecords();
+        if (!timelineRecords.Any())
+        {
+            context.Information("No timeline records found.");
+        }
+        else
+        {
+            foreach (var timelineRecord in timelineRecords)
+            {
+                context.Information("{0}: {1}", timelineRecord.Id, timelineRecord.Name);
+            }
+        }
+    }
+}
+
+[TaskName("Read-BuildArtifacts")]
+public sealed class ReadBuildArtifactsTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        // var build =
+        //     context.AzureDevOpsBuildUsingAzurePipelinesOAuthToken();
+
+        // var artifacts = build.GetArtifacts();
+        // if (!artifacts.Any())
+        // {
+        //     context.Information("No artifacts found.");
+        // }
+        // else
+        // {
+        //     foreach (var artifact in artifacts)
+        //     {
+        //         context.Information("{0}: {1}", artifact.Id, artifact.Name);
+        //     }
+        // }
+    }
+}
+
+[TaskName("Read-BuildTestRuns")]
+public sealed class ReadBuildTestRunsTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        // var build =
+        //     context.AzureDevOpsBuildUsingAzurePipelinesOAuthToken();
+
+        // var testRuns = build.GetTestRuns();
+        // if (!testRuns.Any())
+        // {
+        //     context.Information("No test runs found.");
+        // }
+        // else
+        // {
+        //     foreach (var testRun in testRuns)
+        //     {
+        //         context.Information("{0}", testRun.RunId);
+        //     }
+        // }
+    }
+}
+
 [TaskName("Read-PullRequest")]
 public sealed class ReadPullRequestTask : FrostingTask<BuildContext>
 {
@@ -54,6 +147,10 @@ public sealed class ReadPullRequestTask : FrostingTask<BuildContext>
 
 [TaskName("Default")]
 [IsDependentOn(typeof(ReadBuildTask))]
+[IsDependentOn(typeof(ReadBuildChangesTask))]
+[IsDependentOn(typeof(ReadBuildTimelineRecordsTask))]
+[IsDependentOn(typeof(ReadBuildArtifactsTask))]
+[IsDependentOn(typeof(ReadBuildTestRunsTask))]
 [IsDependentOn(typeof(ReadPullRequestTask))]
 public class DefaultTask : FrostingTask
 {
