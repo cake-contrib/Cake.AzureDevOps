a3eff185211ea0a29d3e002490c4310a335d22c5
Christian Bumann <bumann.christian@gmx.ch>
2023-10-10 21:10:29 +0200

    Fixes #430

---
+++ src/Cake.AzureDevOps.Tests/Pipelines/ArtifactResourceExtensionsTests.cs
@@ -0,0 +1,73 @@
+﻿namespace Cake.AzureDevOps.Tests.Pipelines
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Cake.AzureDevOps.Pipelines;
+    using Cake.Common.Build.AzurePipelines.Data;
+    using Microsoft.TeamFoundation.Build.WebApi;
+    using Shouldly;
+    using Xunit;
+
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public sealed class ArtifactResourceExtensionsTests
+    {
+        public sealed class TheToAzureDevOpsArtifactResourceExtensionMethod
+        {
+            [Fact]
+            public void Should_Return_The_Correct_AzureDevOpsArtifactResource_For_ArtifactResource()
+            {
+                // Given
+                var artifactResource = new ArtifactResource()
+                {
+                    Data = "data",
+                    DownloadUrl = "downloadUrl",
+                    Url = "url",
+                    Type = "FilePath",
+                    Properties = new Dictionary<string, string>()
+                    {
+                        { "foo", "bar" },
+                    },
+                };
+
+                // When
+                var result = artifactResource.ToAzureDevOpsArtifactResource();
+
+                // Then
+                result.Data.ShouldBe(artifactResource.Data);
+                result.DownloadUrl.ShouldBe(artifactResource.DownloadUrl);
+                result.Url.ShouldBe(artifactResource.Url);
+                result.Type.ShouldBe(AzurePipelinesArtifactType.FilePath);
+                result.Properties.Count.ShouldBe(1);
+                result.Properties.First().Key.ShouldBe(artifactResource.Properties.First().Key);
+                result.Properties.First().Value.ShouldBe(artifactResource.Properties.First().Value);
+            }
+
+            [Theory]
+            [InlineData("container", AzurePipelinesArtifactType.Container)]
+            [InlineData("Container", AzurePipelinesArtifactType.Container)]
+            [InlineData("FilePath", AzurePipelinesArtifactType.FilePath)]
+            [InlineData("filepath", AzurePipelinesArtifactType.FilePath)]
+            [InlineData("GitRef", AzurePipelinesArtifactType.GitRef)]
+            [InlineData("gitref", AzurePipelinesArtifactType.GitRef)]
+            [InlineData("TFVCLabel", AzurePipelinesArtifactType.TFVCLabel)]
+            [InlineData("tfvclabel", AzurePipelinesArtifactType.TFVCLabel)]
+            [InlineData("VersionControl", AzurePipelinesArtifactType.VersionControl)]
+            [InlineData("versioncontrol", AzurePipelinesArtifactType.VersionControl)]
+
+            public void Should_Return_The_Correct_AzureDevOpsArtifactResource_Type_EnumValue_Independent_The_ArtifactResource_Type_String_Casing(string typeString, AzurePipelinesArtifactType artifactType)
+            {
+                // Given
+                var artifactResource = new ArtifactResource()
+                {
+                    Type = typeString,
+                };
+
+                // When
+                var result = artifactResource.ToAzureDevOpsArtifactResource();
+
+                // Then
+                result.Type.ShouldBe(artifactType);
+            }
+        }
+    }
+}
